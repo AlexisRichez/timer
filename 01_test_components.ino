b21acb6a6bf537d27d6059785cb79d9c27609762
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2023-11-29 14:45:02 +0100

    Update 01_test_components.ino

--- 01_test_components.ino
+++ 01_test_components.ino
@@ -1,26 +1,27 @@
 #include "LedControl.h"
 
-/*
-Two MAX7221's
-pin 12 DataIn 
-pin 11  CLK 
-pin 10 LOAD 
-*/
+//MAX7219 configuration
 #define PIN_MAX72_DATA_IN 12
 #define PIN_MAX72_CLK 11
 #define PIN_MAX72_LOAD 10
 #define MAX72_NB_DEVICES 2
 
-#define PIN_GREEN_LED 5
-#define PIN_RED_LED 6
+//Yellow led (TAU)
 #define PIN_YELLOW_LED 7
+//Red led (DELTA)
+#define PIN_RED_LED 6
+//Green led (ZETA)
+#define PIN_GREEN_LED 5
+
+//Red led (Between HH, MM, SS)
 #define PIN_RED_COLONS_LED 13
 
-#define BARGRAPH_MAX_LED 8
-
+//Global led configuration
 #define DEFAULT_INTENSITY_LED 8
 
-#define DEFAULT_DELAY 1000
+
+//Debug configuration
+#define DEFAULT_DELAY_MS 1000
 
 LedControl lc = LedControl(PIN_MAX72_DATA_IN,PIN_MAX72_CLK,PIN_MAX72_LOAD,MAX72_NB_DEVICES);
 
@@ -48,59 +49,59 @@ void loop() {
       lc.setChar(0,3,i,false);
       lc.setChar(0,4,i,false);
       lc.setChar(0,5,i,false);   
-      delay(DEFAULT_DELAY);
+      delay(DEFAULT_DELAY_MS);
     }
     
     //Blink red colons
     digitalWrite(PIN_RED_COLONS_LED, HIGH);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     digitalWrite(PIN_RED_COLONS_LED, LOW);
     
     //Blink green led
     digitalWrite(PIN_GREEN_LED, HIGH);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     digitalWrite(PIN_GREEN_LED, LOW); 
 
     //Blink red led
     digitalWrite(PIN_RED_LED, HIGH);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     digitalWrite(PIN_RED_LED, LOW); 
 
     Blink yellow led
     digitalWrite(PIN_YELLOW_LED, HIGH);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     digitalWrite(PIN_YELLOW_LED, LOW); 
 
     //A
     lc.setRow(1,0,B01000000);
     lc.setRow(1,1,B01000000);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     //B
     lc.setRow(1,0,B00100000);
     lc.setRow(1,1,B00100000);      
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     //C
     lc.setRow(1,0,B00010000);
     lc.setRow(1,1,B00010000);
-    delay(DEFAULT_DELAY);    
+    delay(DEFAULT_DELAY_MS);    
     //D
     lc.setRow(1,0,B00001000);
     lc.setRow(1,1,B00001000);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     //E
     lc.setRow(1,0,B00000100);    
     lc.setRow(1,1,B00000100);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     //F
     lc.setRow(1,0,B00000010);
     lc.setRow(1,1,B00000010);
-    delay(DEFAULT_DELAY);    
+    delay(DEFAULT_DELAY_MS);    
     //G
     lc.setRow(1,0,B00000001);
     lc.setRow(1,1,B00000001);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
     //DP
     lc.setRow(1,0,B10000000);
     lc.setRow(1,1,B10000000);
-    delay(DEFAULT_DELAY);
+    delay(DEFAULT_DELAY_MS);
 }
